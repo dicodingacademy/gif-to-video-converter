c198796b7eb155c9f1c6cef1b2c0118a71856941
Dimas Saputra <dimas.maulanads@gmail.com>
2023-02-03 13:46:43 +0700

    add global style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './components/styled/GlobalStyle';
 
 (async () => {
   const root = createRoot(document.getElementById('root'));
-  root.render(<App />);
+  root.render(
+    <>
+      <GlobalStyle />
+      <App />
+    </>,
+  );
 })();
